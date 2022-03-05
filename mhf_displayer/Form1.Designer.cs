78510c4cec196f43d0b1380ee9ee3ca0bd1b7b51
suzaku <89909040+suzaku01@users.noreply.github.com>
2022-03-05 09:10:40 +0900

    init

--- mhf_displayer/Form1.Designer.cs
+++ mhf_displayer/Form1.Designer.cs
@@ -28,12 +28,60 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "30000";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.LimeGreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Label label1;
+    }
 }
--- mhf_displayer/Form1.cs
+++ mhf_displayer/Form1.cs
@@ -1,12 +1,91 @@
+using Memory;
+using System.Configuration;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Linq;
+
 namespace mhf_displayer
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
+        public Form1()
 		{
 			InitializeComponent();
-
-			string str = "aaa";
 		}
-	}
+
+        Mem m = new Mem();
+
+        long adrf;
+        UIntPtr adr4;
+        bool alreadyExist = false;
+        int monsterHPValue;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+			this.BackColor = Color.LimeGreen;
+            this.Location = new Point(265, 90);
+
+            int PID = m.GetProcIdFromName("mhf");
+			if (PID > 0)
+			{
+				m.OpenProcess(PID);
+
+                adrf = m.AoBScan("0F BF D1 31 D0 A3 ?? ?? ?? ?? E9 ?? ?? ?? ??").Result.FirstOrDefault();
+                string adr = adrf.ToString("X8");
+
+                if (adr == "00000000")
+                {
+                    long adr1 = m.AoBScan("0F B7 8a 24 06 00 00 0f b7 ?? ?? ?? c1 c1 e1 0b").Result.FirstOrDefault();
+                    adr1 = adr1 + 58;
+                    long adr2 = adr1 + 5;
+
+                    UIntPtr adr3 = m.CreateCodeCave(adr1.ToString("X8"), new byte[] { 0x0F, 0xBF, 0xD1, 0x31, 0xD0, 0xA3, 0x2A, 0x00, 0x80, 0x00 }, 5, 0x100);
+                    m.WriteMemory(adr2.ToString("X8"), "bytes", "C3");
+
+                    adr4 = adr3 + 20;
+                    string str = adr4.ToString("X8");
+                    byte[] data = Enumerable.Range(0, str.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(str.Substring(x, 2), 16)).ToArray();
+                    Array.Reverse(data, 0, data.Length);
+                    UIntPtr adr5 = adr3 + 6;
+                    m.WriteBytes(adr5, data);
+
+                    timer1.Start();
+                }
+                else
+                {
+                    alreadyExist = true;
+                    adrf = adrf + 20;
+                    timer1.Start();
+                }
+            }
+            else
+            {
+				MessageBox.Show("Launch game first");
+                this.Close();
+
+            }
+
+		}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (alreadyExist)
+            {
+                monsterHPValue = m.Read2Byte(adrf.ToString("X8"));
+                label1.Text = monsterHPValue.ToString();
+            }
+            else
+            {
+                monsterHPValue = m.Read2Byte(adr4.ToString("X8"));
+                label1.Text = monsterHPValue.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			this.Close();
+		}
+    }
 }
